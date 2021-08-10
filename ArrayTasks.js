10acfe3cc755f9695c47711082f114770a63d5d4
Aditya Uphade <aditya.uphade@morningstar.com>
2021-08-10 15:31:24 +0530

    Create an array containing 10 numbers of 3 digits find the 2nd largest & smallest number

--- ArrayTasks.js
+++ ArrayTasks.js
@@ -1,12 +1,23 @@
-//Task1 - Create a 3 digit random number
+//Task1 - Create a 3 digit random number, Store it in an array
 var num = [];
 
 function getRandomNumbers(count){
     for(let i=0; i < count; i++){
             num.push(Math.floor(100 + Math.random() * 900));
     }
-    console.log(num);
+    console.log(`\nThe Original Array: ${num}\n`);
 }
-
-
 getRandomNumbers(10);
+
+//Task1 - find the 2nd largest & 2nd smallest number
+var arr = num;
+
+const lar1 = Math.max(...arr);
+arr.splice(arr.indexOf(lar1), 1);
+
+const small1 = Math.min(...arr);
+arr.splice(arr.indexOf(small1), 1);
+
+console.log(`\nThe largest number is: ${lar1}\nThe smallest number is: ${small1}\n`);
+
+console.log(`The 2nd largest number is: ${Math.max(...arr)}\nThe 2nd smallest number is: ${Math.min(...arr)}`);
