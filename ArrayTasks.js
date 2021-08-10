583c9a30e00f20b68a3e4e7ae38ef07116acdfee
Aditya Uphade <aditya.uphade@morningstar.com>
2021-08-10 18:56:49 +0530

    Prime Factors of a given number

--- ArrayTasks.js
+++ ArrayTasks.js
@@ -1,29 +1,50 @@
-//Task1 - Create a 3 digit random number, Store it in an array
-var num = [];
+// //Task1 - Create a 3 digit random number, Store it in an array
+// var num = [];
 
-function getRandomNumbers(count){
-    for(let i=0; i < count; i++){
-            num.push(Math.floor(100 + Math.random() * 900));
-    }
-    console.log(`\nThe Original Array: ${num}\n`);
-}
-getRandomNumbers(10);
+// function getRandomNumbers(count){
+//     for(let i=0; i < count; i++){
+//             num.push(Math.floor(100 + Math.random() * 900));
+//     }
+//     console.log(`\nThe Original Array: ${num}\n`);
+// }
+// getRandomNumbers(10);
 
-//Task1 - find the 2nd largest & 2nd smallest number
-var arr = [...num];
+// //Task1 - find the 2nd largest & 2nd smallest number
+// var arr = [...num];
 
-const lar1 = Math.max(...arr);
-arr.splice(arr.indexOf(lar1), 1);
+// const lar1 = Math.max(...arr);
+// arr.splice(arr.indexOf(lar1), 1);
 
-const small1 = Math.min(...arr);
-arr.splice(arr.indexOf(small1), 1);
+// const small1 = Math.min(...arr);
+// arr.splice(arr.indexOf(small1), 1);
 
-console.log(`\nThe largest number is: ${lar1}\nThe smallest number is: ${small1}\n`);
+// console.log(`\nThe largest number is: ${lar1}\nThe smallest number is: ${small1}\n`);
 
-console.log(`The 2nd largest number is: ${Math.max(...arr)}\nThe 2nd smallest number is: ${Math.min(...arr)}`);
+// console.log(`The 2nd largest number is: ${Math.max(...arr)}\nThe 2nd smallest number is: ${Math.min(...arr)}`);
 
 
-//Task2 - find the 2nd largest & 2nd smallest number by sorting
- var sorted_num = num.sort(function(a, b) {return a - b;});
- console.log(`\n\n\nSorted Array: ${sorted_num}`);
- console.log(`The 2nd Largest number: ${sorted_num.splice(1,1)} & The 2nd Smallest number: ${sorted_num.splice(sorted_num.length-2, 1)}`)
+// //Task2 - find the 2nd largest & 2nd smallest number by sorting
+//  var sorted_num = num.sort(function(a, b) {return a - b;});
+//  console.log(`\n\n\nSorted Array: ${sorted_num}`);
+//  console.log(`The 2nd Largest number: ${sorted_num.splice(1,1)} & The 2nd Smallest number: ${sorted_num.splice(sorted_num.length-2, 1)}`)
+
+ //Task3 - Find Prime factors of a given number
+
+ var a = 13;
+ var pFactors = [];
+ divisor = 2;
+
+ function getFactors(n){
+     while (n>=2) {
+         if (n % divisor == 0) {
+             pFactors.push(divisor);
+             n = n/divisor;
+         } else {
+             divisor++;
+         }   
+     }
+     return pFactors;
+ }
+
+ console.log(`The prime factors of ${a} are: ${getFactors(a)}`);
+
